3909b73d8d581b3aac36158d6f26e34ed3b1e67f
Anton Lytvynenko <antonlytvynenko@gmail.com>
2015-09-21 22:26:32 +0300

    hello world
    first actual commit

---
+++ test.py
@@ -0,0 +1,13 @@
+print ("Hello World from PyCharm")
+
+def function():
+    print ("function was called")
+
+function()
+
+# This function prints 100 vslues
+def ptinr100():
+    for i in range (101, 1):
+        print (i)
+
+ptinr100()
